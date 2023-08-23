c3d39c7d48232a70682c62ee9a3da011bd0ab2ce
Stanislav <stazilastas@gmail.com>
2023-08-23 09:52:48 -0500

    Flashlight 1.0

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var onOutlet: UIButton!
+    
+    @IBOutlet weak var offOutlet: UIButton!
+    @IBOutlet weak var labelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        onOutlet.setTitleColor(UIColor.black, for: .normal)
+        offOutlet.setTitleColor(UIColor.black, for: .normal)
     }
-
-
+    
+    
+    @IBAction func onAction(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+        labelOutlet.text = "ON"
+        labelOutlet.textColor = UIColor.black
+    }
+    @IBAction func offAction(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+        labelOutlet.text = "OFF"
+        labelOutlet.textColor = UIColor.white
+    }
 }
 
