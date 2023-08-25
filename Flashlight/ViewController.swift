ccf88aa66530bcd0bffeaa25754e28338502910a
stasik <stazilastas@gmail.com>
2023-08-24 22:32:28 -0500

    Flashlight 3.0

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -14,23 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var offOutlet: UIButton!
     @IBOutlet weak var labelOutlet: UILabel!
     
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var switchOutlet: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
+        onOutlet.tintColor = UIColor.white
+        offOutlet.tintColor = UIColor.black
         // Do any additional setup after loading the view.
-        onOutlet.setTitleColor(UIColor.black, for: .normal)
-        offOutlet.setTitleColor(UIColor.black, for: .normal)
     }
     
+
     
     @IBAction func onAction(_ sender: Any) {
         view.backgroundColor = UIColor.white
         labelOutlet.text = "ON"
         labelOutlet.textColor = UIColor.black
+        switchOutlet.setOn(true, animated: true)
     }
     @IBAction func offAction(_ sender: Any) {
         view.backgroundColor = UIColor.black
         labelOutlet.text = "OFF"
         labelOutlet.textColor = UIColor.white
+        switchOutlet.setOn(false, animated: true)
+    }
+    @IBAction func switchAction(_ sender: Any) {
+        if(switchOutlet.isOn){
+            onAction(self)
+        }
+        else { offAction(self)}
     }
 }
 
